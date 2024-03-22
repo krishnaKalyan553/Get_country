ef7c4523b7d32b712027889289635e9b1f0773a6
Work@Astrokun <nyshadhamkrishnakalyan2@gmail.com>
2024-03-22 13:30:30 +0530

    added db file

--- geo_targetting/geo_middleware.py
+++ geo_targetting/geo_middleware.py
@@ -1,13 +1,16 @@
 import geoip2.database
-
+from django.conf import settings
+import os
+base_dir = settings.BASE_DIR
+db_path = os.path.join(base_dir, 'GeoLite2-City.mmdb')
 class GeoLocationMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        self.reader = geoip2.database.Reader(r'/home/workastrokun/Downloads/GeoLite2-City.mmdb')
+        self.reader = geoip2.database.Reader(db_path)
 
     def __call__(self, request):
-        # ip_address = request.META.get('REMOTE_ADDR')
-        ip_address = '2405:201:c41f:e1ad:2208:4488:39b5:2970'
+        ip_address = request.META.get('REMOTE_ADDR')
+        # ip_address = '2405:201:c41f:e1ad:2208:4488:39b5:2970'
         try:
             location = self.reader.city(ip_address)
             country = location.country.name
